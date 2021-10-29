57422fb92dc6fc7d3e7aef0f9e8c67c76313f155
sattaku1538 <choose.chose.chosen@gmail.com>
2021-10-29 05:30:50 +0000

    finished

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,22 +1,46 @@
 class TodolistsController < ApplicationController
+  
   def new
     @list = List.new
-    # 投稿用の空の箱を作っているため、DefCreste部分でListと表示している。
   end
 
   def create
-    # １. データを新規登録するためのインスタンス作成
-    list = List.new(list_params)
-    # ２. データをデータベースに保存するためのsaveメソッド実行。List.newから作成されたデータをlistへ渡す。
-    list.save
-    # ３. トップ画面へリダイレクト
-    redirect_to '/top'
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to todolist_path(@list.id)
+    else
+      render :new
+    end
+  end
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to todolists_path  # 投稿一覧画面へリダイレクト 
   end
 
   private
-  # ストロングパラメータ
+  
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
-
+  
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = '474c72ae311d12d10d6f0fc93d377b9c41e258848984af2fb8bf1593c4abb55a95459b8611081ba792aacf41f0280d6aa148b23ed88b7e038a2d4688c58f70c6'
